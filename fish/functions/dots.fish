1767d3aa3e1c7d8bd01e65d4c4e556bc7f28d32e
Daniel Tucker <daniel@dantuck.com>
2021-12-16 07:25:48 -0700

    Fix dots command

--- fish/functions/dots.fish
+++ fish/functions/dots.fish
@@ -1,18 +1,20 @@
 function dots -d "dots management utility"
     switch $argv[1]
-    case 'u'
+    case 'update'
         pushd $DOTFILES
         git pull
 
         fish $DOTFILES/fish/scripts/bootstrap.fish
         popd
 
+    case 'cd'
     case '*'
         pushd $DOTFILES
         git pull
     end
 end
 
-set -l dots_commands update
+set -l dots_commands update cd
 complete -f --command dots
 complete -f --command dots -n "not __fish_seen_subcommand_from $dots_commands" -a update -d 'update dots'
+complete -f --command dots -n "not __fish_seen_subcommand_from $dots_commands" -a cd -d 'cd into dots and pull'
