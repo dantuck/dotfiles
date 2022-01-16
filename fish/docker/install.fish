7463ab16c6674f1c20166800ab105096dd8d1202
Daniel Tucker <daniel@dantuck.com>
2022-01-16 14:23:20 -0700

    Fixed docker commands

--- fish/docker/install.fish
+++ fish/docker/install.fish
@@ -1,3 +1,3 @@
-abbr -a docker_rm_all "docker rm \`docker ps -a -q\`"
-abbr -a docker_rmi_all "docker rmi \`docker images -q\`"
-abbr -a docker_rmi_dangling "docker rmi \`docker images -qa -f 'dangling=true'\`"
+abbr -a docker_rm_all "docker ps -a -q | xargs docker rm"
+abbr -a docker_rmi_all "docker images -a -q | xargs docker rmi -f"
+abbr -a docker_rmi_dangling "docker image prune -af"
