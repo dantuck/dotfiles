0e9f6819f5f8f611832e78f165dc3cf6f5e695e5
Daniel Tucker <gitlab@dantuck.com>
2022-01-23 19:19:33 +0000

    Removing tmux and replacing with zellij

---
+++ zellij/install.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/env fish
+
+switch (uname)
+case Darwin
+    link_file config.yaml "$HOME/Application Support/org.Zellij-Contributors.Zellij/config.yml" backup
+case '*'
+    link_file config.yaml $HOME/.config/zellij/config.yml backup
