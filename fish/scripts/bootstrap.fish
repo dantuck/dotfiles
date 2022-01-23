d71da8a6a4d8bb455ce54e409036c684bd1b3ee2
Daniel Tucker <daniel@dantuck.com>
2022-01-23 12:18:26 -0700

    Remove commented link_file from bootstrap

--- fish/scripts/bootstrap.fish
+++ fish/scripts/bootstrap.fish
@@ -58,25 +58,6 @@ function setup_gitconfig
 		or abort 'failed to setup git'
 end
 
-# function link_file -d "links a file keeping a backup"
-# 	echo $argv | read -l old new backup
-# 	if test -e $new
-# 		set newf (readlink $new)
-# 		if test "$newf" = "$old"
-# 			success "skipped $old"
-# 			return
-# 		else
-# 			mv $new $new.$backup
-# 				and success moved $new to $new.$backup
-# 				or abort "failed to backup $new to $new.$backup"
-# 		end
-# 	end
-# 	mkdir -p (dirname $new)
-# 		and ln -sf $old $new
-# 		and success "linked $old to $new"
-# 		or abort "could not link $old to $new"
-# end
-
 function install_dotfiles
 	for src in $DOTFILES_ROOT/fish/**/*.symlink
 		link_file $src $HOME/.(basename $src .symlink) backup
