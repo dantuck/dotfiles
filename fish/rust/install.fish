6b8566ef030f4141d621fdd7b066d0b59b9b10a0
Daniel Tucker <daniel.tucker@divvypay.com>
2022-01-25 06:52:41 -0700

    Add updated rust config

--- fish/rust/install.fish
+++ fish/rust/install.fish
@@ -4,5 +4,8 @@ if command -qa rustup
 end
 
 if command -qa cargo
+    link_file $DOTFILES/fish/rust/config.toml $HOME/.cargo/config.toml backup
+		or abort cargo config
+
     set -Ua fish_user_paths $HOME/.cargo/bin
 end
