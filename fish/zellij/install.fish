6c98c5029727a3d61bf417a6eebbf7e981fb59b2
Daniel Tucker <code@dantuck.com>
2022-10-25 14:22:41 -0600

    Update zellij config

--- fish/zellij/install.fish
+++ fish/zellij/install.fish
@@ -1,8 +1,25 @@
 #!/usr/bin/env fish
 
-switch (uname)
-case Darwin
-    link_file config.yaml "$HOME/Application Support/org.Zellij-Contributors.Zellij/config.yml" backup
-case '*'
-    link_file config.yaml $HOME/.config/zellij/config.yml backup
+function test_version
+    set regex_semver '[0-9]+\.[0-9]+\.[0-9]+'
+
+    set zellij_version (zellij --version | string match --regex $regex_semver)
+    set zellij_version_info (string split . $zellij_version)
+    set zellij_version_major $zellij_version_info[1]
+    set zellij_version_minor $zellij_version_info[2]
+    set zellij_version_patch $zellij_version_info[3]
+
+    if test $zellij_version_minor -lt 32
+        abort "please update zellij: `cargo install --locked zellij`"
+    end
+end
+
+if command -qa zellij
+    for f in $DOTFILES/fish/scripts/helpers
+        set -Up fish_function_path $f
+    end
+
+    test_version
+
+    set -Ux ZELLIJ_CONFIG_DIR $DOTFILES/fish/zellij
 end
