f43093fdc6ee9c927774d4dcf1fc017b507c9ff6
Daniel Tucker <daniel.tucker@divvypay.com>
2022-11-04 08:11:14 -0600

    Fix of setting the path

--- fish/rover/install.fish
+++ fish/rover/install.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 if test -f $HOME/.rover/bin/rover
-	fish_add_path -aP $HOME/.rover/bin
+	set -Ua fish_user_paths  $HOME/.rover/bin
 
     set -Ux APOLLO_TELEMETRY_DISABLED 1
 end
