5a5e0e59c99edf5965eb9794f2be0d0b536efa65
Daniel Tucker <daniel.tucker@divvypay.com>
2023-01-17 07:12:00 -0700

    Fixed zola install for system that does not have flatpak

--- fish/zola/install.fish
+++ fish/zola/install.fish
@@ -5,15 +5,17 @@ end
 
 function remove_zola
     functions --erase zola
+    funcsave zola
 end
 
 if ! command -qs flatpak
     remove_zola
+    exit 0
 end
 
 if ! set command_output (flatpak info org.getzola.zola)
     remove_zola
+    exit 0
 end
-
 
 funcsave zola
--- fish/zola/zola.fish
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env fish
-function zola
-    flatpak run org.getzola.zola $argv
-end
