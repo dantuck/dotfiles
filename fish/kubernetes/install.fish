3ea78715a394a230d3852eff72e7be6cac2d3719
Daniel Tucker <code@dantuck.com>
2023-12-29 13:35:30 -0700

    Adds oxker as a command option

--- fish/kubernetes/install.fish
+++ fish/kubernetes/install.fish
@@ -16,6 +16,57 @@ if command -qa kubens
     abbr -a kn kubens
 end
 
+function oxker -d "oxker: install it if you want it"
+    echo (set_color blue)"oxker:"(set_color brblack)" command not installed"(set_color normal)
+    echo (set_color brblack)"  A simple tui to view & control docker containers"(set_color normal)
+    echo
+
+    function read_confirm
+        set -l response
+        trap "echo -e '\nAborted by user.'; return 1" INT
+    
+        while true
+            read -P "$argv[1] (y/N): " -l response
+    
+            switch $response
+                case "y" "Y"
+                    trap - INT
+                    return 0
+                case "n" "N" ""
+                    trap - INT
+                    return 1
+            end
+        end
+    end
+
+    if command -qa cargo
+        echo (set_color blue)"[i]> Install with:"(set_color normal)
+        echo (set_color blue)"-------------"(set_color normal)
+        
+        echo "cargo install oxker"
+        
+        echo (set_color blue)"-------------"(set_color normal)
+        echo
+
+        if read_confirm "Do you want it installed for you?"
+            command cargo install oxker
+
+            functions --erase oxker &> /dev/null
+            funcsave oxker &> /dev/null
+
+            prevd
+        end
+    end
+
+    return 1
+end
+
+funcsave oxker &> /dev/null
+
+if ! command -qs oxker
+	exit 0
+end
+
 abbr -a k kubectl
 abbr -a sk 'kubectl -n kube-system'
 abbr -a kg 'kubectl get'
